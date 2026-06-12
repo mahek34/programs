d96651fb4f9c2f3fa318169718c86fdd0b900c6c
Your Name <sinduriyamahek@gmail.com>
2026-06-12 13:25:58 -0400

    EMPLOTEE.C

---
+++ BASIES/EMPLOYEE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*variable 
+emp-id
+salary
+bonus
+*/
+int main(){
+    int emp_id=234;
+    long salary=5670000;
+    int bonus=6500;
+
+    printf("\nemployee id is: %d",emp_id);
+    printf("\nemployee salary and bonus is:%ld",salary+bonus);
+
+
+
+
+
+    return 0;
+}
