c55300337aec852ca2e64c4b2cffd8e2538a924a
Your Name <sinduriyamahek@gmail.com>
2026-06-12 13:48:14 -0400

    ORDER.C

---
+++ BASIES/ORDER.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int order_id=345;
+    float price=45.70;
+    int quantity=5;
+    printf("\norder id is:%d",order_id);
+    printf("\ntotal price is:%f",price*quantity);
+
+
+
+
+
+
+
+
+    return 0;
+
+}
