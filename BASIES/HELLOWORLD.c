c0f6f04ff2e8dc1f93be9288b436b94ae5bf6b71
Your Name <sinduriyamahek@gmail.com>
2026-06-13 17:30:47 -0400

    S-5

--- BASIES/HELLOWORLD.c
+++ BASIES/HELLOWORLD.c
@@ -5,4 +5,3 @@ int main()
     printf("hello world");
     return 0;
 }
-
---
+++ BASIES/SQUARESIDE.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    long long moblie = 9876543210;
+    char grade='B';
+    printf("\n%lld",moblie);
+    printf("\n%c",grade);
+    
+
+    return 0;
+
+}
