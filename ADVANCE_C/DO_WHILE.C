e8e5c3d2f55c801055dd6293438100ff9c08ac5b
Your Name <sinduriyamahek@gmail.com>
2026-06-27 13:17:16 -0400

    DO_WHILE.C

---
+++ ADVANCE_C/DO_WHILE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i=10;
+
+    do
+    {
+        printf("loop executed! the value of i is :%d\n",i);
+    
+        /* code */
+    } while (i < 5);
+
+    return 0;
+    
+}
