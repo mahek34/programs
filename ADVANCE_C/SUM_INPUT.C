7e35b793fb5b2e367dda20af8c50291a9cc967c1
Your Name <sinduriyamahek@gmail.com>
2026-06-27 09:47:31 -0400

    SUM_INPUT.C

---
+++ ADVANCE_C/SUM_INPUT.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+
+    printf("enter first number:");
+    scanf("%d",&num1);
+
+    printf("enter sconed number:");
+    scanf("%d",&num2);
+
+    sum= num1+ num2;
+
+    printf("total sum of %d and%d is:%d",num1,num2,sum);
+
+    return 0;
+
+}
