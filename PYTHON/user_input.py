fae5ea2e1125b22038b79f0ace141a3fca6deabd
Your Name <sinduriyamahek@gmail.com>
2026-06-28 10:29:00 -0400

    add python user_input.py

---
+++ PYTHON/user_input.py
@@ -0,0 +1,8 @@
+name = input("enter your name:")
+age = int(input("enter your age:"))
+marks =float(input("enter your marks"))
+
+print("\n---STUDENT DETAILS---")
+print("NAME :",name)
+print("AGE :",age)
+print("MARKS :",marks)
