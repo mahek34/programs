688322887e6c8bc1e45276de71a3790083d9378c
Your Name <sinduriyamahek@gmail.com>
2026-07-12 22:09:10 -0400

    name_case.py

---
+++ PYTHON/name_case.py
@@ -0,0 +1,21 @@
+#2-3
+message="hello rohit,would you like to learn python today?"
+print(message)
+#2-4
+name="rohit sharma"
+print(name.lower())
+print(name.upper())
+#2-5
+quote ='Albert Einstein once said ,"A person who never made a mistake never tired anything new"'
+print(quote)
+#2-5
+famous_name ="Albert Einstein"
+quote_by_famous = "You never fail until you stop trying."
+print(f'{famous_name} once said, "{quote_by_famous}"')
+#2-6
+name = "\t\n  VIRAT KOHIL  \n\t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
