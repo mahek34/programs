325081c00aca5efe0969a5cb1783868f56953a9d
Your Name <sinduriyamahek@gmail.com>
2026-07-14 11:01:14 -0400

    name_case.py

--- PYTHON/name_case.py
+++ PYTHON/name_case.py
@@ -8,14 +8,25 @@ print(name.upper())
 #2-5
 quote ='Albert Einstein once said ,"A person who never made a mistake never tired anything new"'
 print(quote)
-#2-5
+#2-6
 famous_name ="Albert Einstein"
 quote_by_famous = "You never fail until you stop trying."
 print(f'{famous_name} once said, "{quote_by_famous}"')
-#2-6
+#2-7
 name = "\t\n  VIRAT KOHIL  \n\t"
 print(name)
 print(name.lstrip())
 print(name.rstrip())
 print(name.strip())
+#2-8
+print(5+3)
+print(10-2)
+print(2*4)
+print(64/8)
+#2-9
+favorite_number=8
+message3=(f"my favourite number is :{favorite_number}")
+print(message3)
+
+
 
